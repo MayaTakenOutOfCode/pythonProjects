381d160d358c9c6691057a56e9b2e7c0283f99ab
Maya <115114380+MayaTakenOutOfCode@users.noreply.github.com>
2023-06-25 00:59:45 +0200

    Update generator.py
    I just added voice affs typing and changing them to audio files from gtts

--- AffsGen/generator.py
+++ AffsGen/generator.py
@@ -1,7 +1,14 @@
 import random
 import gtts
+from pynput.keyboard import Key, Controller
+
+import speech_recognition as sr
+import pyautogui as keyboard
+
 
 print("++++++++++++++++++++++++++++++\n")
+
+
 
 def generateSubAudio(af,af2):
     
@@ -17,36 +24,83 @@ def generateSubAudio(af,af2):
         affs2 += ele
         tts = gtts.gTTS(affs2, slow=False )
         tts.save("affs2.wav")
-
-af = []
-
-num1 = int(input(
+def useKeyboard():
+    af = []
+    num1 = int(input(
     "Enter a number that represents an amount of affirmations you're gonna use: "
     ))
+    if num1 <=0:
+        print("You gotta write positive and not equal 0")
+    else:
+        for i in range(0,num1):
+            ele = str(input("Affirmation(subliminal):"))
+            af.append(ele + ".")
 
-if num1 <=0:
-    print("You gotta write positive and not equal 0")
-else:
-    for i in range(0,num1):
-        ele = str(input("Affirmation(subliminal):"))
+    af2 = []
+
+    num2 = int(input("Enter a number that represents amount of booster affirmations you will use: "))
+
+    if num2 <=0:
+        print("You gotta write positive and not equal 0 ")
+    else:
+        for k in range(0,num2):
+            ele2 = str(input("Affirmation(boost):"))
     
-        af.append(ele + ".")
-
-af2 = []
-
-num2 = int(input("Enter a number that represents amount of booster affirmations you will use: "))
-
-if num2 <=0:
-    print("You gotta write positive and not equal 0 ")
-else:
-    for k in range(0,num2):
-        ele2 = str(input("Affirmation(boost):"))
+            af2.append(ele2 + ".")
+    random.shuffle(af)
+    random.shuffle(af2)
+    generateSubAudio(af, af2)    
     
-        af2.append(ele2 + ".")
+def chooseMethod():
+    print("Choose k - keyboard affs, v - voice affs\n")
+    key = input('Your choice: ')
+    if(key == 'k'):
+        useKeyboard()
+    elif(key == 'v'):
+        useVoice()
+    else:
+        print("This is not avalible for you, the key you can only use are: k, v")
+        
+def useVoice():
+    
+    af = []
 
-random.shuffle(af)
-random.shuffle(af2)
-generateSubAudio(af, af2)
+    num1 = int(input(
+        "Enter a number that represents an amount of affirmations you're gonna use: "
+    ))
+
+    if num1 <=0:
+        print("You gotta write positive and not equal 0")
+    else:
+        for i in range(0,num1):
+            r = sr.Recognizer()
+            with sr.Microphone() as source:
+                audio = r.listen(source)
+            inp = r.recognize_google(audio)
+            ele = inp+"\n";
+    
+            af.append(ele + ".")
+
+    af2 = []
+
+    num2 = int(input("Enter a number that represents amount of booster affirmations you will use: "))
+
+    if num2 <=0:
+        print("You gotta write positive and not equal 0 ")
+    else:
+        for k in range(0,num2):
+            r2 = sr.Recognizer()
+            with sr.Microphone() as source2:
+                audio2 = r.listen(source2)
+            inp2 = r.recognize_google(audio2)
+            ele2 = inp2+"\n";
+    
+            af2.append(ele2 + ".")
+    random.shuffle(af)
+    random.shuffle(af2)
+    generateSubAudio(af, af2)
+chooseMethod()
+#print(af, "\n",af2)
 
 print("++++++++++++++++++++++++++++++")
 print("Thx for using me!")
